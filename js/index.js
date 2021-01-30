05ab61fed20528bf1a363dc861535ed97f8f47f3
Rakibul Hasan <rakibulhasan745101@gmail.com>
2021-01-30 15:33:12 +0600

    fourth-portfolio-as-assignment

---
+++ js/index.js
@@ -0,0 +1,30 @@
+function eventHandler(element, isIncrement) {
+    var inputField = document.querySelector(element);
+    var inputValue = parseInt(inputField.value);
+    var inputNewValue = isIncrement === true ? inputValue + 1 : inputValue > 0 ? inputValue - 1 : inputValue;
+    inputField.value = inputNewValue;
+    calculation();
+}
+
+function calculation(){
+    var firstClassInputField = document.querySelector('#first-class-ticket input');
+    var firstClassInputValue = parseInt(firstClassInputField.value);
+
+    var economyTypeInputField = document.querySelector('#economy-type-ticket input');
+    var economyTypeInputValue = parseInt(economyTypeInputField.value);
+
+    var subtotal = (firstClassInputValue) * 150 + (economyTypeInputValue * 100);
+    document.getElementById('subtotal').innerText = '$' + subtotal;
+
+    var vat = subtotal * 0.1;
+    document.getElementById('vat').innerText = '$' + vat;
+
+    var total = subtotal + vat;
+    document.getElementById('total').innerText = '$' + total;
+}
+
+document.querySelector('button.btn-style').addEventListener('click', function(){
+    var firstClassInputValue = parseInt(document.querySelector('#first-class-ticket input').value);
+    var economyTypeInputValue = parseInt(document.querySelector('#economy-type-ticket input').value);
+    window.alert('You bought ' + firstClassInputValue + ' ticket of first class and ' + economyTypeInputValue + ' ticket of economy type.')
+})
